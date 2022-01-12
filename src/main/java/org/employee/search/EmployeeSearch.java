a9e722ad3cdf59849754d6fdab087bb1a2288f94
Aubrhey <aubrhey.caisip@ph.ncs-i.com>
2022-01-12 09:53:18 +0800

    Initial Commit

---
+++ src/main/java/org/employee/search/EmployeeSearch.java
@@ -0,0 +1,12 @@
+package org.employee.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSearch {
+
+    public static void main(String[] args){
+        SpringApplication.run(EmployeeSearch.class, args);
+    }
+}
